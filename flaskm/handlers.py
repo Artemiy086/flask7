2ec0d9ff1c8a048125e4c9d1264dc5f2918ce1de
TS1UL <artemy.tsiulin@gmail.com>
2024-03-31 15:52:03 +0300

    oi

--- flaskm/handlers.py
+++ flaskm/handlers.py
@@ -9,7 +9,11 @@ def index():
     return render_template("index.html")
 
 
-@bp.route("/distribution")
-def distribution():
-    team = ["Ридди", "Петя", "Коля", "Маша", "Иван", "Гриша", "Саша", "Миша"]
-    return render_template("list_prof.html", team=team)
+@bp.route("/table/<male>/<int:age>")
+def table(male, age):
+    key = male + ["", "_child"][age > 21]
+    color = {"male": "#0080FF", "male_child": "#0000FF",
+                  "female": "#FF9933", "female_child": "#FF8000"}[key]
+    scr = "img/" + key + ".jpg"
+    print(scr)
+    return render_template("table.html", color=color, scr=scr)
